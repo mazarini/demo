93a329e02ed9ac3e619100860745cc4bf1f6a47c
mazarini <pascal.riva@gmail.com>
2023-01-07 20:44:43 +0100

    Add boostrap and adapt template Add some datas to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Kernel;
 
 class HomeController extends AbstractController
 {
@@ -12,7 +13,8 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'php_version' => phpversion(),
+            'symfony_version' => Kernel::VERSION,
         ]);
     }
 }
